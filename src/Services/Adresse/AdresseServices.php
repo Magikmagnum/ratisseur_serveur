fd8aad3cedfb47db9dbbf59e3e95422bfcf3c570
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2025-02-27 20:34:37 +0000

    fix: suppression des lignes inutil

--- src/Services/Adresse/AdresseServices.php
+++ src/Services/Adresse/AdresseServices.php
@@ -2,27 +2,19 @@
 
 namespace App\Services\Adresse;
 
-use App\Entity\Pays;
-use App\Entity\Ville;
-use App\DTO\AdresseDTO;
 use App\Entity\Adresse;
 use App\Helpers\EntityHelper;
-use App\Services\DTOServices;
-use App\Traits\EntityCrudSingleTrait;
-use App\Repository\PaysRepository;
-use App\Helpers\HttpResponseHelper;
-use App\Repository\VilleRepository;
 use App\Traits\EntityHydratorTrait;
 use App\Repository\AdresseRepository;
+use App\Traits\EntityCrudSingleTrait;
 use App\Controller\AbstractController;
-use App\Exception\ValidationException;
 use App\Services\Adresse\VilleServices;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Services\Adresse\AdresseInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use App\Services\Interfaces\ServiceInterface;
 
-class AdresseServices extends AbstractController implements AdresseInterface
+/**
+ * @implements ServiceInterface<Adresse>
+ */
+class AdresseServices extends AbstractController implements ServiceInterface
 {
     use EntityHydratorTrait;
     use EntityCrudSingleTrait;
@@ -41,14 +33,12 @@ class AdresseServices extends AbstractController implements AdresseInterface
     /**
      * Mappe les données fournies dans une entité Adresse.
      *
-     * @param \App\Entity\Adresse $adresse L'entité Adresse à hydrater.
+     * @param Adresse $adresse L'entité Adresse à hydrater.
      * @param array $data Les données à mapper dans l'entité.
-     * @return \App\Entity\Adresse L'entité Adresse hydratée.
+     * @return Adresse L'entité Adresse hydratée.
      */
-    private function mapDataToEntity(Adresse $adresse, array $data): Adresse
+    public function mapDataToEntity(Object $adresse, array $data): Adresse
     {
-        $user = $this->getUser();
-        $adresseUser = $adresse->getUser();
         // Assurez-vous que l'utilisateur est défini
         if (!$adresse->getId()) {
             $adresse->addUser($this->getUser());
@@ -78,17 +68,12 @@ class AdresseServices extends AbstractController implements AdresseInterface
     /**
      * @return Adresse
      */
-    public function getEntity(): Adresse|null
+    public function getEntity(): Adresse
     {
-        $adresse = $this->getUser()->getAdresse();
+        $adresse = $this->getUser()->getadresse();
+        if (!$adresse) {
+            $adresse = new Adresse();
+        }
         return $adresse;
-    }
-
-    /**
-     * @return Adresse
-     */
-    private function newEntity(): Adresse
-    {
-        return new Adresse();
     }
 }
